2335d81244fcc040342a5b68c302a8667278a3a0
MiloszWci <pangaska1@gmail.com>
2023-01-03 16:32:06 +0100

    fixes

--- components/navbar/indexnavbar.php
+++ components/navbar/indexnavbar.php
@@ -4,14 +4,16 @@
         <img src="img/logo.png" alt="Logo">
         <h4><a href="/">Student Life</a></h4>
     </div>
-    <a id="btn-logout" href="logout.php">Wyloguj</a>
+
     <div class="dropdown">
      <button class="dropbtn">Menu</button>
         <div class="dropdown-content">
             <a href="events.php">Wydarzenia</a>
             <a href="#">Koła naukowe</a>
             <a href="#">Wskazówki</a>
-            <a href = "#">Pomoc</a>
+            <a href ="#">Pomoc</a>
+            <a href="logout.php">Wyloguj</a>
         </div>
     </div>
+    
 </nav>
