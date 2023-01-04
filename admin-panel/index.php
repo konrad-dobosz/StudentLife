d29dab7872050a01d723a98c4a9e5a2e4a5ad60f
konrad-dobosz <crisps.taffies.0w@icloud.com>
2023-01-04 18:33:45 +0100

    Add initial admin panel

---
+++ admin-panel/index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+if ((!isset($_SESSION['logged'])) && ($_SESSION['logged'] != true)) {
+    header('Location: login.php');
+    exit();
+}
+
+require_once "../db.php";
+?>
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin panel | StudentLife</title>
+
+    <link rel="stylesheet" href="css/admin.css">
+</head>
+
+<body>
+    <main>
+        <section>
+            <form>
+                <h4>Lista wydarzeń</h4>
+                <?php
+                if ($result = $connection->query(sprintf("SELECT * FROM events LIMIT 10"))) {
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<a href="event-edit.php?id=' . $row['id'] . '" class="event-row">' . $row['name'] . '</a>';
+                        }
+                    } else {
+                        echo '<p>Brak wydarzeń</p>';
+                    }
+                }
+                ?>
+            </form>
+        </section>
+    </main>
+</body>
+
+</html>
