50a3da219ea761d09f6bc37bbdbb9b8d79375858
konrad-dobosz <crisps.taffies.0w@icloud.com>
2023-01-08 18:45:22 +0100

    Add remove event button and add error message when fields are empty

--- admin-panel/event.php
+++ admin-panel/event.php
@@ -36,6 +36,8 @@ if ((!isset($_SESSION['logged'])) && ($_SESSION['logged'] != true)) {
 
                     <form method="post">
                         <?php
+                        $error_msg = '';
+
                         switch ($_GET['mode']) {
                             case 'create':
                         ?>
@@ -53,27 +55,32 @@ if ((!isset($_SESSION['logged'])) && ($_SESSION['logged'] != true)) {
                                 <?php
                                 break;
                             case 'edit':
-                                if ($result = $connection->query(sprintf("SELECT * FROM events WHERE id=" . $_GET['id'] . " LIMIT 1"))) {
-                                    if ($result->num_rows > 0) {
-                                        $row = $result->fetch_assoc();
-                                        $date_start = new DateTime($row['date_start']);
-                                        $date_end = new DateTime($row['date_end']);
+                                if ($_GET['id'] == '') {
+                                    echo '<h2>Nie podano id</h2>';
+                                } else {
+                                    if ($result = $connection->query(sprintf("SELECT * FROM events WHERE id=" . $_GET['id'] . " LIMIT 1"))) {
+                                        if ($result->num_rows > 0) {
+                                            $row = $result->fetch_assoc();
+                                            $date_start = new DateTime($row['date_start']);
+                                            $date_end = new DateTime($row['date_end']);
                                 ?>
-                                        <label>Nazwa wydarzenia <input type="text" value="<?php echo $row['name']; ?>" name="name"></label>
-                                        <label>Adres <input type="text" value="<?php echo $row['address']; ?>" name="address"></label>
-                                        <label>Data rozpoczęcia<input type="datetime-local" name="date_start" id="" value="<?php echo $date_start->format('Y-m-d\TH:i'); ?>"></label>
-                                        <label>Data zakończenia<input type="datetime-local" name="date_end" id="" value="<?php echo $date_end->format('Y-m-d\TH:i'); ?>"></label>
+                                            <label>Nazwa wydarzenia <input type="text" value="<?php echo $row['name']; ?>" name="name" required></label>
+                                            <label>Adres <input type="text" value="<?php echo $row['address']; ?>" name="address" required></label>
+                                            <label>Data rozpoczęcia<input type="datetime-local" name="date_start" id="" value="<?php echo $date_start->format('Y-m-d\TH:i'); ?>" required></label>
+                                            <label>Data zakończenia<input type="datetime-local" name="date_end" id="" value="<?php echo $date_end->format('Y-m-d\TH:i'); ?>" required></label>
 
-                                        <label>
-                                            Opis
-                                            <textarea name="description" id="" cols="30" rows="10"><?php echo $row['description']; ?></textarea>
-                                        </label>
-                                        <button type="submit" name="submit">Zapisz</button>
+                                            <label>
+                                                Opis
+                                                <textarea name="description" id="" cols="30" rows="10" required><?php echo $row['description']; ?></textarea>
+                                            </label>
+                                            <button type="submit" name="submit">Zapisz</button>
+                                            <button type="submit" id="event-remove" name="submit-remove">Usuń wydarzenie</button>
 
                         <?php
 
-                                    } else {
-                                        echo '<h2>Brak wydarzenia o podanym id</h2>';
+                                        } else {
+                                            echo '<h2>Brak wydarzenia o podanym id</h2>';
+                                        }
                                     }
                                 }
                                 break;
@@ -90,17 +97,34 @@ if ((!isset($_SESSION['logged'])) && ($_SESSION['logged'] != true)) {
                             $date_start = $_POST['date_start'];
                             $date_end = $_POST['date_end'];
 
-                            if (empty($id) || empty($name) || empty($address) || empty($description || empty($date_start)) || empty($date_end)) {
-                                // TODO: Add field error handling
+                            if (empty($name) || empty($address) || empty($description || empty($date_start)) || empty($date_end)) {
+                                $error_msg = 'Należy wypełnić każde pole!';
                             } else {
                                 if ($_GET['mode'] == 'edit') {
-                                    $connection->query("UPDATE events SET name = '$name', address = '$address', description = '$description', date_start = '$date_start', date_end = '$date_end' WHERE id = '$id'");
+                                    if (empty($id)) {
+                                        $error_msg = 'Nie podano id';
+                                    } else {
+                                        $connection->query("UPDATE events SET name = '$name', address = '$address', description = '$description', date_start = '$date_start', date_end = '$date_end' WHERE id = '$id'");
+                                        header('Location: index.php');
+                                    }
                                 } else if ($_GET['mode'] == 'create') {
                                     $connection->query("INSERT INTO events SET name = '$name', address = '$address', description = '$description', date_start = '$date_start', date_end = '$date_end'");
                                 }
                             }
                         }
+
+                        if (isset($_POST['submit-remove'])) {
+                            $id = $_GET['id'];
+
+                            $connection->query("DELETE FROM events WHERE id='$id'");
+                            header('Location: index.php');
+                        }
                         ?>
+                        <?php if ($error_msg != '') : ?>
+                            <p style="color: red;">
+                                <?php echo $error_msg; ?>
+                            </p>
+                        <?php endif; ?>
                     </form>
         </section>
     </main>
